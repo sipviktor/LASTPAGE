5f56b1375125909de3617cc2299e6dd1d9c52876
sipviktor <96287934+sipviktor@users.noreply.github.com>
2022-04-19 12:29:42 +0200

    Update gen.js

--- js/gen.js
+++ js/gen.js
@@ -6,48 +6,76 @@ xhr.onload = function (){
     j = JSON.parse(j);
     console.log(j);
     const blocks = document.getElementById("blocks-p");
-    var c = 0;
-    var g = 0;
-    if (c != 0 && g == 0){
-     j.sort(function(a, b){
-      return a.cena - b.cena;
-    });
-  } else if(g != 0 && c == 0){
-    j.sort(function(a, b){
-     if(a.nazev < b.nazev){
-       return -1;
-     }
-     if(a.nazev > b.nazev){
-       return 1;
-     }
-     return 0;
-    });
-  }
-   for(let i=0; i < j.length; i++){
-    const block = document.createElement("div");
-    block.setAttribute("class", "block-p");
-    block.setAttribute("id", "block-p");
-    blocks.appendChild(block);
-    const ob = document.createElement("img");
-    ob.src = "../img/" + j[i]["idzbozi"] + ".png"
-    block.appendChild(ob);
-    const specs = document.createElement("div");
-    specs.setAttribute("class", "specs-p");
-    specs.setAttribute("id", "specs-p");
-    block.appendChild(specs);
-    const price = document.createElement("p");
-    price.setAttribute("id", "specs-p");
-    price.innerHTML = j[i]["cena"] + " Kč";
-    specs.appendChild(price);
-    const name = document.createElement("p");
-    name.setAttribute("id", "brand");
-    name.innerHTML = j[i]["znacka"] + " - " + j[i]["nazev"];
-    specs.appendChild(name);
-    const stock = document.createElement("p");
-    stock.setAttribute("id", "stock");
-    stock.innerHTML = j[i]["pocet"] + " ks";
-    specs.appendChild(stock);
-   }
-   
+    const g = document.getElementById("C").onclick = function cena(){
+      j.sort(function(a, b){
+        return a.cena - b.cena;
+      });
+
+      for(let i=0; i < j.length; i++){
+        const block = document.createElement("div");
+        block.setAttribute("class", "block-p");
+        block.setAttribute("id", "block-p");
+        blocks.appendChild(block);
+        const ob = document.createElement("img");
+        ob.src = "../img/" + j[i]["idzbozi"] + ".png"
+        block.appendChild(ob);
+        const specs = document.createElement("div");
+        specs.setAttribute("class", "specs-p");
+        specs.setAttribute("id", "specs-p");
+        block.appendChild(specs);
+        const price = document.createElement("p");
+        price.setAttribute("id", "specs-p");
+        price.innerHTML = j[i]["cena"] + " Kč";
+        specs.appendChild(price);
+        const name = document.createElement("p");
+        name.setAttribute("id", "brand");
+        name.innerHTML = j[i]["znacka"] + " - " + j[i]["nazev"];
+        specs.appendChild(name);
+        const stock = document.createElement("p");
+        stock.setAttribute("id", "stock");
+        stock.innerHTML = j[i]["pocet"] + " ks";
+        specs.appendChild(stock);
+        }
+    };    
+    
+    const c = document.getElementById("A").onclick = function abeceda(){
+      j.sort(function(a, b){
+        if(a.nazev < b.nazev){
+          return -1;
+        }
+        if(a.nazev > b.nazev){
+          return 1;
+        }
+        return 0;
+       });
+
+      for(let i=0; i < j.length; i++){
+        const block = document.createElement("div");
+        block.setAttribute("class", "block-p");
+        block.setAttribute("id", "block-p");
+        blocks.appendChild(block);
+        const ob = document.createElement("img");
+        ob.src = "../img/" + j[i]["idzbozi"] + ".png"
+        block.appendChild(ob);
+        const specs = document.createElement("div");
+        specs.setAttribute("class", "specs-p");
+        specs.setAttribute("id", "specs-p");
+        block.appendChild(specs);
+        const price = document.createElement("p");
+        price.setAttribute("id", "specs-p");
+        price.innerHTML = j[i]["cena"] + " Kč";
+        specs.appendChild(price);
+        const name = document.createElement("p");
+        name.setAttribute("id", "brand");
+        name.innerHTML = j[i]["znacka"] + " - " + j[i]["nazev"];
+        specs.appendChild(name);
+        const stock = document.createElement("p");
+        stock.setAttribute("id", "stock");
+        stock.innerHTML = j[i]["pocet"] + " ks";
+        specs.appendChild(stock);
+        }
+    };
+    
+    
 };
 xhr.send();
